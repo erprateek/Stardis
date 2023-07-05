585a4265a39e3ef93d33ca5d62c3c64babb4e08d
Prateek Tandon <ptandon1@andrew.cmu.edu>
2023-07-04 21:31:24 -0700

    Update BinaryTree.py

--- stardis/BinaryTree.py
+++ stardis/BinaryTree.py
@@ -1,63 +1,197 @@
 from stardis import BinaryTree
 
+"""
+BinaryTree Module
+
+This module implements a binary tree data structure using the BinaryTree class.
+A binary tree is a hierarchical structure where each node has at most two children, referred to as the left child and the right child.
+
+Classes:
+- BinaryTree: Represents a binary tree data structure.
+
+"""
+
 class BinaryTree:
+    """
+    BinaryTree Class
+
+    A class representing a binary tree data structure.
+
+    Methods:
+    - insert(item): Inserts an item into the binary tree.
+    - highest(): Returns the highest value in the binary tree.
+    - lowest(): Returns the smallest value in the binary tree.
+    - delete(value): Deletes a value from the binary tree.
+
+    """
+
     class Node:
-        def __init__(self,value):
+        """
+        Node Class
+
+        A class representing a node in the binary tree.
+
+        Attributes:
+        - value: The value of the node.
+        - left: The reference to the left child node.
+        - right: The reference to the right child node.
+        - level: The level of the node in the binary tree.
+
+        """
+
+        def __init__(self, value):
+            """
+            Node initialization.
+
+            Args:
+            - value: The value to be stored in the node.
+
+            """
             self.value = value
             self.left = None
             self.right = None
             self.level = None
-            
+
     def __init__(self):
+        """
+        BinaryTree initialization.
+
+        Initializes an empty binary tree.
+
+        """
         self.root = None
         self.size = 0
-        
+
     def insert(self, item):
-        if self.value == item:
-            return #we do nothing because the item is already here
+        """
+        Insert an item into the binary tree.
+
+        Args:
+        - item: The item to be inserted.
+
+        """
+        if self.root is None:
+            self.root = self.Node(item)
         else:
-            if item < self.value:
-                if self.left != None:
-                    self.left.insert(item)
-                else:
-                    self.left = self.Node(item)
+            self._insert_recursive(item, self.root)
+
+    def _insert_recursive(self, item, current_node):
+        """
+        Recursively insert an item into the binary tree.
+
+        Args:
+        - item: The item to be inserted.
+        - current_node: The current node being considered during the insertion.
+
+        """
+        if item < current_node.value:
+            if current_node.left is not None:
+                self._insert_recursive(item, current_node.left)
             else:
-                if self.right != None:
-                    self.right.insert(item)
-                else:
-                    self.right = self.Node(item)
-                    
+                current_node.left = self.Node(item)
+        else:
+            if current_node.right is not None:
+                self._insert_recursive(item, current_node.right)
+            else:
+                current_node.right = self.Node(item)
+
     def highest(self):
-        """Returns the highest value in the tree"""
-        if self.right != None:
-            return self.right.highest()
+        """
+        Return the highest value in the binary tree.
+
+        Returns:
+        The highest value in the binary tree.
+
+        """
+        if self.root is not None:
+            return self._highest_recursive(self.root)
         else:
-            return self.value
-    
+            return None
+
+    def _highest_recursive(self, current_node):
+        """
+        Recursively find the highest value in the binary tree.
+
+        Args:
+        - current_node: The current node being considered during the traversal.
+
+        Returns:
+        The highest value in the binary tree.
+
+        """
+        if current_node.right is not None:
+            return self._highest_recursive(current_node.right)
+        else:
+            return current_node.value
+
     def lowest(self):
-        """Returns the smallest value in the tree"""
-        if self.left != None:
-            return self.left.lowest()
+        """
+        Return the smallest value in the binary tree.
+
+        Returns:
+        The smallest value in the binary tree.
+
+        """
+        if self.root is not None:
+            return self._lowest_recursive(self.root)
         else:
-            return self.value
-    
+            return None
+
+    def _lowest_recursive(self, current_node):
+        """
+        Recursively find the smallest value in the binary tree.
+
+        Args:
+        - current_node: The current node being considered during the traversal.
+
+        Returns:
+        The smallest value in the binary tree.
+
+        """
+        if current_node.left is not None:
+            return self._lowest_recursive(current_node.left)
+        else:
+            return current_node.value
+
     def delete(self, value):
-        #### If the current node has the value
-        if value == self.value:
-            if self.left != None:
-                #### Set the value to the highest value in the left subtree
-                #### Delete that value
-                self.value = self.left.highest()
-                self.left.delete(self.value)
-            elif self.right != None:
-                self.value = self.right.lowest()
-                self.right.delete(self.value)
+        """
+        Delete a value from the binary tree.
+
+        Args:
+        - value: The value to be deleted.
+
+        Returns:
+        The root node of the modified binary tree.
+
+        """
+        if self.root is not None:
+            self.root = self._delete_recursive(value, self.root)
+
+    def _delete_recursive(self, value, current_node):
+        """
+        Recursively delete a value from the binary tree.
+
+        Args:
+        - value: The value to be deleted.
+        - current_node: The current node being considered during the deletion.
+
+        Returns:
+        The node resulting from the deletion.
+
+        """
+        if value == current_node.value:
+            if current_node.left is not None:
+                current_node.value = self._highest_recursive(current_node.left)
+                current_node.left = self._delete_recursive(current_node.value, current_node.left)
+            elif current_node.right is not None:
+                current_node.value = self._lowest_recursive(current_node.right)
+                current_node.right = self._delete_recursive(current_node.value, current_node.right)
             else:
                 return None
         else:
-            """Find which subtree has the value"""
-            if value < self.value and self.left != None:
-                self.left = self.left.delete(value)
-            if value > self.value and self.right != None:
-                self.right = self.right.delete(value)
-            return self
+            if value < current_node.value and current_node.left is not None:
+                current_node.left = self._delete_recursive(value, current_node.left)
+            if value > current_node.value and current_node.right is not None:
+                current_node.right = self._delete_recursive(value, current_node.right)
+            return current_node
+
